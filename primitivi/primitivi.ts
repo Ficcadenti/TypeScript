0438faf864b8b27d0801d27506291d4793815c7a
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-01-31 13:28:50 +0100

    Ancora sui tipi primitivi

--- primitivi/primitivi.ts
+++ primitivi/primitivi.ts
@@ -1,5 +1,39 @@
 let isDone: boolean = false;
-let i: number = 1;
+let myname: string = "Raffaele";
+let eta: number = 43;
+let listN: Array<number> = [1, 2, 3];
+let listS: string[] = ['1', '2', '3'];
+let obj: object = {};
+let params: [string, number];
+enum DAYS { LUNEDI = 1, MARTEDI, MERCOLEDI, GIOVEDI, VENERDI, SABATO, DOMENICA };
+let giorno: DAYS = DAYS.MERCOLEDI;
+
+function showParams(p: [string, number]) {
+    console.log("showParams : " + p[0] + ';' + p[1]);
+}
+
+function isWorkingDay(day: DAYS) {
+    if (day == DAYS.SABATO || day == DAYS.DOMENICA) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+isDone = 4 > 5;
+params = ["Raffaele", 43];
 
 console.log("boolean: " + isDone);
-console.log("number: " + i);
+console.log("string: " + myname);
+console.log("number: " + eta);
+console.log("Array<number>: " + listN);
+console.log("string[]: " + listS);
+console.log("object: " + obj);
+console.log("tuple: " + params);
+console.log("enum: " + DAYS);
+console.log("giorno: " + giorno);
+console.log("giorno: " + DAYS[1]);
+console.log(isWorkingDay(DAYS.LUNEDI));
+
+showParams(params);
+
